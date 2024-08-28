b623cd4041833a4a025483e85c3698e47cfb7cd1
MiningCryptoLive <celldr69@gmail.com>
2024-08-28 08:57:18 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'https://doge.solopool.us:4335',
-    STRATUM_URL: 'doge.solopool.us:4577'
+    API_URL: 'https://doge.solopool.us:4001',
+    STRATUM_URL: 'doge.solopool.us'
 };
